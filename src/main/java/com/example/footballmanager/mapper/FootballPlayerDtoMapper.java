f2444dce61375f505401478f84f12758e110037d
Anna Popova <anitapopova1996@gmail.com>
2022-12-03 19:23:49 +0200

    created new Mappers for FootballTeam and FootballPlayer and removed old.

---
+++ src/main/java/com/example/footballmanager/mapper/FootballPlayerDtoMapper.java
@@ -0,0 +1,38 @@
+package com.example.footballmanager.mapper;
+
+import com.example.footballmanager.dto.request.FootballPlayerRequestDto;
+import com.example.footballmanager.dto.response.FootballPlayerResponseDto;
+import com.example.footballmanager.model.FootballPlayer;
+import com.example.footballmanager.service.FootballTeamService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FootballPlayerDtoMapper implements RequestDtoMapper<FootballPlayerRequestDto, FootballPlayer>,
+        ResponseDtoMapper<FootballPlayerResponseDto, FootballPlayer> {
+    private final FootballTeamService teamService;
+
+    public FootballPlayerDtoMapper(FootballTeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    @Override
+    public FootballPlayer mapToModel(FootballPlayerRequestDto dto) {
+        FootballPlayer player = new FootballPlayer();
+        player.setName(dto.getName());
+        player.setAge(dto.getAge());
+        player.setYearsExperience(dto.getYearsExperience());
+        player.setFootballTeam(teamService.getById(dto.getFootballTeamId()));
+        return player;
+    }
+
+    @Override
+    public FootballPlayerResponseDto mapToDto(FootballPlayer player) {
+        FootballPlayerResponseDto responseDto = new FootballPlayerResponseDto();
+        responseDto.setId(player.getId());
+        responseDto.setName(player.getName());
+        responseDto.setAge(player.getAge());
+        responseDto.setYearsExperience(player.getYearsExperience());
+        responseDto.setFootballTeamId(player.getFootballTeam().getId());
+        return responseDto;
+    }
+}
