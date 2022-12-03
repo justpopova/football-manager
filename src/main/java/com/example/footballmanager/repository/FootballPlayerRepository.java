ca97588fa6165f4b20393d308ea4741145674cd0
Anna Popova <anitapopova1996@gmail.com>
2022-12-03 14:04:17 +0200

    added  football player repository

--- src/main/java/com/example/footballmanager/repository/FootballPlayerRepository.java
+++ src/main/java/com/example/footballmanager/repository/FootballPlayerRepository.java
@@ -2,6 +2,8 @@ package com.example.footballmanager.repository;
 
 import com.example.footballmanager.model.FootballPlayer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FootballPlayerRepository extends JpaRepository<FootballPlayer, Long> {
 }
