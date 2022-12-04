ead56c578f2e15409145c98996ac2cc31e1a5851
Anna Popova <anitapopova1996@gmail.com>
2022-12-04 19:49:41 +0200

    custom exceptions added

---
+++ src/main/java/com/example/footballmanager/exception/FinancialTransactionException.java
@@ -0,0 +1,7 @@
+package com.example.footballmanager.exception;
+
+public class FinancialTransactionException extends RuntimeException {
+    public FinancialTransactionException(String message) {
+        super(message);
+    }
+}
