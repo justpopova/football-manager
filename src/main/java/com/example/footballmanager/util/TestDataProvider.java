e857b439b888cfb5e43416386df5ba6a8bc4df76
Anna Popova <anitapopova1996@gmail.com>
2022-12-04 19:57:11 +0200

    Refactored name and moved to another package

--- src/main/java/com/example/footballmanager/util/TestDataProvider.java
+++ src/main/java/com/example/footballmanager/util/TestDataProvider.java
@@ -1,50 +1,27 @@
-package com.example.footballmanager.controller;
+package com.example.footballmanager.util;
 
 import com.example.footballmanager.model.FootballPlayer;
 import com.example.footballmanager.model.FootballTeam;
-import com.example.footballmanager.model.bankAccount.BankAccount;
-import com.example.footballmanager.service.BankAccountService;
-import com.example.footballmanager.service.FootballManagerService;
 import com.example.footballmanager.service.FootballPlayerService;
 import com.example.footballmanager.service.FootballTeamService;
 import jakarta.annotation.PostConstruct;
-
-import java.math.BigDecimal;
 import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 @Component
-public class InjectorController {
+public class TestDataProvider {
     private final FootballTeamService footballTeamService;
 
     private final FootballPlayerService footballPlayerService;
 
-    private final BankAccountService bankAccountService;
-    private final FootballManagerService managerService;
-
-    public InjectorController(FootballTeamService footballTeamService,
-                              FootballPlayerService footballPlayerService,
-                              BankAccountService bankAccountService,
-                              FootballManagerService managerService) {
+    public TestDataProvider(FootballTeamService footballTeamService,
+                            FootballPlayerService footballPlayerService) {
         this.footballTeamService = footballTeamService;
         this.footballPlayerService = footballPlayerService;
-        this.bankAccountService = bankAccountService;
-        this.managerService = managerService;
     }
 
     @PostConstruct
     public void inject() {
-        BankAccount firstTeamAccount = new BankAccount();
-        firstTeamAccount.setAccountNumber("123456");
-        firstTeamAccount.setAmount(BigDecimal.valueOf(1500000));
-        bankAccountService.save(firstTeamAccount);
-
-        BankAccount secondTeamAccount = new BankAccount();
-        secondTeamAccount.setAccountNumber("456789");
-        secondTeamAccount.setAmount(BigDecimal.valueOf(2000000));
-        bankAccountService.save(secondTeamAccount);
-
         FootballPlayer player1 = new FootballPlayer();
         player1.setName("Greg");
         player1.setAge(32);
@@ -71,14 +48,12 @@ public class InjectorController {
 
         FootballTeam team1 = new FootballTeam();
         team1.setName("Blizzard");
-        team1.setBankAccount(firstTeamAccount);
         team1.setCommission(0.05);
         team1.setPlayers(List.of(player1, player3));
         footballTeamService.save(team1);
 
         FootballTeam team2 = new FootballTeam();
         team2.setName("Go Rogers!");
-        team2.setBankAccount(secondTeamAccount);
         team2.setCommission(0.07);
         team2.setPlayers(List.of(player2, player4));
         footballTeamService.save(team2);
