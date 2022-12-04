6fe015988ee5437ef6b0ced75d90d125d2b4fc3f
Anna Popova <anitapopova1996@gmail.com>
2022-12-04 19:54:51 +0200

    added validation

--- src/main/java/com/example/footballmanager/dto/request/FootballTeamRequestDto.java
+++ src/main/java/com/example/footballmanager/dto/request/FootballTeamRequestDto.java
@@ -1,11 +1,17 @@
 package com.example.footballmanager.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public class FootballTeamRequestDto {
+    @NotNull
     private String name;
     private List<Long> playersIds;
     private Long bankAccountId;
+    @DecimalMin("0.0") @DecimalMax("0.1")
     private double commission;
 
     public String getName() {
