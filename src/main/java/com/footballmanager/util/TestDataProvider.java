e236226690980d3af91bc29e996da0f91cbd3c8d
Anna Popova <anitapopova1996@gmail.com>
2022-12-21 17:25:20 +0200

    refactored entities data

--- src/main/java/com/footballmanager/util/TestDataProvider.java
+++ src/main/java/com/footballmanager/util/TestDataProvider.java
@@ -5,7 +5,11 @@ import com.footballmanager.model.FootballTeam;
 import com.footballmanager.service.FootballPlayerService;
 import com.footballmanager.service.FootballTeamService;
 import jakarta.annotation.PostConstruct;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,37 +28,37 @@ public class TestDataProvider {
     public void inject() {
         FootballPlayer player1 = new FootballPlayer();
         player1.setName("Greg");
-        player1.setAge(32);
-        player1.setYearsExperience(5);
+        player1.setAge(LocalDate.of(1990, 9, 10));
+        player1.setYearsExperience(LocalDate.of(2020,8,18));
         footballPlayerService.save(player1);
 
         FootballPlayer player2 = new FootballPlayer();
         player2.setName("Bob");
-        player2.setAge(29);
-        player2.setYearsExperience(4);
+        player2.setAge(LocalDate.of(1986, 6, 10));
+        player2.setYearsExperience(LocalDate.of(2016,2,10));
         footballPlayerService.save(player2);
 
         FootballPlayer player3 = new FootballPlayer();
         player3.setName("Edwin");
-        player3.setAge(36);
-        player3.setYearsExperience(8);
+        player3.setAge(LocalDate.of(1992, 1, 8));
+        player3.setYearsExperience(LocalDate.of(2019,10,10));
         footballPlayerService.save(player3);
 
         FootballPlayer player4 = new FootballPlayer();
         player4.setName("Gregory");
-        player4.setAge(40);
-        player4.setYearsExperience(10);
+        player4.setAge(LocalDate.of(1982, 11, 15));
+        player4.setYearsExperience(LocalDate.of(2010,1,21));
         footballPlayerService.save(player4);
 
         FootballTeam team1 = new FootballTeam();
         team1.setName("Blizzard");
-        team1.setCommission(0.05);
+        team1.setCommission(BigDecimal.valueOf(0.05));
         team1.setPlayers(List.of(player1, player3));
         footballTeamService.save(team1);
 
         FootballTeam team2 = new FootballTeam();
         team2.setName("Go Rogers!");
-        team2.setCommission(0.07);
+        team2.setCommission(BigDecimal.valueOf(0.07));
         team2.setPlayers(List.of(player2, player4));
         footballTeamService.save(team2);
 
